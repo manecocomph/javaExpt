f137536b6ec4beb0bc5a33bcf0eaba1d38acca8b
Eric Tian <xiatian@ebay.com>
2021-10-22 07:21:04 -0700

    update ImplicitThreadPool

--- src/main/java/com/tianxiaohui/expt/thread/ImplicitThreadPool.java
+++ src/main/java/com/tianxiaohui/expt/thread/ImplicitThreadPool.java
@@ -11,34 +11,37 @@ public class ImplicitThreadPool {
 	public static void main(String[] args) {
 		List<String> names = Arrays.asList("Daniel", "Barry", "Eric", "Yuan");
 
-		// 相当于 for loop
+		// 相当于 for loop. 这里都是当前 main 函数的主线程来执行这些 task. 不包括 call 里面的
 		List<Callable<String>> tasks0 = names.stream().map(name -> {
+			System.out.println("Before make callable task, run by " + Thread.currentThread().getName());
 			return new Callable<String>() {
 				public String call() throws Exception {
-					System.out.println("Run by " + Thread.currentThread().getName());
+					System.out.println("In callable task0, run by " + Thread.currentThread().getName());
 					return name.toUpperCase();
 				}
 			};
 		}).collect(Collectors.toList());
 
-		// 多线程合作 做这点事
+		// 多线程合作 做这点事. 由 ForkJoinPool.commonPool-worker 线程池执行
 		List<Callable<String>> tasks1 = names.parallelStream().map(name -> {
+			System.out.println("Before make callable task, in parallelStream, run by " + Thread.currentThread().getName());
 			return new Callable<String>() {
 				public String call() throws Exception {
-					System.out.println("Run by " + Thread.currentThread().getName());
+					System.out.println("In callable task1, run by " + Thread.currentThread().getName());
 					return name.toUpperCase();
 				}
 			};
 		}).collect(Collectors.toList());
 
+		
 		CompletableFuture.supplyAsync(() -> {
-			System.out.println("Run by " + Thread.currentThread().getName());
+			System.out.println("0 - Run by " + Thread.currentThread().getName());
 			return tasks1;
 		}).thenApplyAsync(tasks -> {
-			System.out.println("Run by " + Thread.currentThread().getName());
+			System.out.println("1 - Run by " + Thread.currentThread().getName());
 			return tasks.stream().map(callable -> {
 				try {
-					System.out.println("Run by " + Thread.currentThread().getName());
+					System.out.println("2 - Run by " + Thread.currentThread().getName());
 					return callable.call();
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -51,5 +54,5 @@ public class ImplicitThreadPool {
 		}).join();
 	}
 
-
+	//以上的所有 Callable task 全部没有被执行
 }
